0ae7193a9c28d2d563aa05e9dc5ffc5bd56f3acb
Vince Gonzalez <Seventito777@gmail.com>
2026-01-05 10:35:01 -0700

    Added minor lines

--- public/js/rankTrackerClient.js
+++ public/js/rankTrackerClient.js
@@ -11,8 +11,8 @@ window.players.forEach((player) => {
 });
 
 const margin = { top: 50, right: 20, bottom: 50, left: 50 };
-const width = (800 * 1.5) - margin.left - margin.right;
-const height = (470 * 1.5) - margin.top - margin.bottom;
+const width = 800 * 1.5 - margin.left - margin.right;
+const height = 470 * 1.5 - margin.top - margin.bottom;
 
 // Create SVG container
 const svg = d3
@@ -155,10 +155,27 @@ lines.forEach((line) => {
   }
 });
 
+// Light dashed lines every 100 MMR (excluding multiples of 400)
+const minorLines = d3
+  .range(Math.ceil(yMin / 100) * 100, yMax, 100)
+  .filter((v) => v % 400 !== 0);
+
+svg
+  .selectAll(".minor-mmr-line")
+  .data(minorLines)
+  .enter()
+  .append("line")
+  .attr("class", "minor-mmr-line")
+  .attr("x1", 0)
+  .attr("x2", width)
+  .attr("y1", (d) => yScale(d))
+  .attr("y2", (d) => yScale(d))
+  .attr("stroke", "#888888ff")
+  .attr("stroke-width", 1)
+  .attr("stroke-dasharray", "2 4");
+
 // Legend
-const legend = svg
-  .append("g")
-  .attr("transform", `translate(${width - 80}, 0)`);
+const legend = svg.append("g").attr("transform", `translate(${width - 80}, 0)`);
 
 const legendItem = legend
   .selectAll(".legend-item")
