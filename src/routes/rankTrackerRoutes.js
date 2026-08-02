1af1160627cf2c28ee995fc6f25c3db71e282af6
Vince Gonzalez <Seventito777@gmail.com>
2026-08-02 07:50:55 -0700

    Riot Verification

--- src/routes/rankTrackerRoutes.js
+++ src/routes/rankTrackerRoutes.js
@@ -1,10 +1,14 @@
 const express = require("express");
+const path = require("path");
 const router = express.Router();
 const rankTrackerController = require("../controllers/rankTrackerController");
 
 router.get("/", rankTrackerController.getPlayers);
 router.get("/players", rankTrackerController.getPlayersJSON);
 router.get("/logs", rankTrackerController.getLogsJSON);
+router.get("/riot.txt", (req, res) => {
+  res.sendFile(path.join(__dirname, "../../public/txt/riot.txt"));
+});
 
 // Export router
 module.exports = router;
